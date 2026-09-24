43bd9dd15126894b0caac93b45fa6e5c4f0fe3ba
Asuka Langley Soryu <zavoxpy@gmail.com>
2026-09-24 13:17:23 +0700

    Update my_utils.py

--- src/my_utils.py
+++ src/my_utils.py
@@ -1,7 +1,40 @@
 import ffmpeg
 import numpy as np
 import gradio as gr
+from contextlib import suppress
+from urllib.parse import urlparse, parse_qs
 
+
+def get_youtube_video_id(url, ignore_playlist=True):
+    """
+    Examples:
+    http://youtu.be/SA2iWivDJiE
+    http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    http://www.youtube.com/embed/SA2iWivDJiE
+    http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    """
+    query = urlparse(url)
+    if query.hostname == 'youtu.be':
+        if query.path[1:] == 'watch':
+            return query.query[2:]
+        return query.path[1:]
+
+    if query.hostname in {'www.youtube.com', 'youtube.com', 'music.youtube.com'}:
+        if not ignore_playlist:
+            # use case: get playlist id not current video in playlist
+            with suppress(KeyError):
+                return parse_qs(query.query)['list'][0]
+        if query.path == '/watch':
+            return parse_qs(query.query)['v'][0]
+        if query.path[:7] == '/watch/':
+            return query.path.split('/')[1]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+
+    # returns None for invalid YouTube url
+    return None
 
 def raise_exception(error_msg, is_webui):
     if is_webui:
